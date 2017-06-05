e7b5cdc838dfc0653604caa65a488d2cc2e48ade
Ryuki Uetsugu <u.ryuki1997@gmail.com>
2017-06-05 17:28:15 +0900

    ゲージが隠れるように修正

---
+++ mlab-unity/Assets/JOKER/Scripts/Novel/Libs/GageObject.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI; 
+
+
+public class GageObject:MonoBehaviour{
+
+
+	void Start() {
+
+	}
+		
+
+	public static void SwitchPoints(string param) {
+
+		Slider _slider;
+		float _points = 0.0f;
+
+		_slider = GameObject.Find("Slider").GetComponent<Slider>();
+		_slider.gameObject.SetActive(true);
+
+
+		switch (param) {
+
+		case "0":
+			_points = 0.1f;
+			_slider.value = _points;
+			break;
+
+		case "1":
+			_points = 0.2f;
+			_slider.value = _points;
+			break;
+
+
+		case "2":
+			_points = 0.3f;
+			_slider.value = _points;
+			break;
+
+
+		case "3":
+			_points = 0.4f;
+			_slider.value = _points;
+			break;
+
+
+		default:
+			break;
+		}
+	}
+
+	public static void HideGage(){
+		Slider _slider;
+
+		_slider = GameObject.Find("Slider").GetComponent<Slider>();
+		_slider.gameObject.SetActive(false);
+	}
+}
+
---
+++ mlab-unity/Assets/JOKER_GAME/Plugins/Gage.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI; 
+
+namespace Novel{
+
+
+	/*	
+--------------
+
+ゲージの増減処理
+[gage]
+points="n"
+
+n = 1, 2, 3
+
+
+ゲージを隠す（デフォルトでゲージは表示されてる）
+[hgage]
+
+
+[_doc]
+--------------------
+ */
+	 	public class GageComponent:AbstractComponent
+	 	{
+		  	public GageComponent ()
+		  	{
+			  
+			   	this.arrayVitalParam = new List<string> {
+				   	"points",
+				   	};
+
+			   	this.originalParam = new Dictionary<string, string>() {
+				   	{"points",""},
+				  	};
+
+			 	}
+
+		  	public override void start ()
+		  	{
+
+			   	string chosenNumber = this.param ["points"];
+
+
+			   	//変数に結果を格納
+				GageObject.SwitchPoints(chosenNumber);
+
+
+			   	//次のシナリオに進む処理
+			   	this.gameManager.nextOrder ();
+
+			  	}
+
+		 	}
+
+
+	 	public class HgageComponent:AbstractComponent
+	 	{
+		  	public HgageComponent ()
+		  	{
+
+			}
+
+		  	public override void start ()
+		  	{
+				GageObject.HideGage();
+			   	this.gameManager.nextOrder ();
+
+			}
+
+		 }
+
+
+}
+
+
